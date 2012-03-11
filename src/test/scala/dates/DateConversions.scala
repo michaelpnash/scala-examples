6fbeb6c58fb4b981b60bd030768fdccc3632aec4
Michael Nash <mnash@jglobal.com>
2012-03-11 10:57:50 -0600

    Additional formats supported

--- src/test/scala/dates/DateConversions.scala
+++ src/test/scala/dates/DateConversions.scala
@@ -1,10 +1,10 @@
 package dates
 
 import java.util.Date
-import java.text.{ParseException, SimpleDateFormat, DateFormat}
+import java.text.{ParseException, SimpleDateFormat}
 
 class DateConvert(val origin: String) {
-  val formats = List(DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT), new SimpleDateFormat("yyyy/MM/dd"), new SimpleDateFormat("MM/dd/yyyy"))
+  val formats = List(new SimpleDateFormat("MMM dd, yyyy"), new SimpleDateFormat("yyyy/MM/dd"), new SimpleDateFormat("MM/dd/yyyy"))
 
   def asDate: Date = {
     if (origin == null) return null
@@ -27,14 +27,16 @@ import java.util.Calendar._
 class DateConversionsTests extends FunSpec {
   describe("the date convert class") {
     it ("can convert a string into the corresponding date explicitly") {
-      //setup
       val expected = new GregorianCalendar(2010, JANUARY, 1).getTime
       val origin = "2010/01/01"
       val converter = new DateConvert(origin)
-      //execute
       val actual = converter.asDate
-      //assert
       assert(actual.getTime === expected.getTime)
+    }
+    it ("can convert a string in several different formats into a date") {
+      val expected = new GregorianCalendar(2010, JANUARY, 1).getTime
+      assert(new DateConvert("01/01/2010").asDate.getTime === expected.getTime)
+      assert(new DateConvert("Jan 1, 2010").asDate.getTime === expected.getTime)
     }
     it ("will throw an exception if the string cannot be converted to a date") {
       intercept[IllegalArgumentException] {
@@ -46,6 +48,12 @@ class DateConversionsTests extends FunSpec {
       val expected = new GregorianCalendar(2010, JANUARY, 1).getTime
       val origin = "2010/01/01"
       assert(origin.asDate.getTime === expected.getTime)
+    }
+    it ("can be used to implicitly convert a string to a date") {
+      implicit def wrapString(s: String) = new DateConvert(s).asDate
+      def getTimeFromDate(date: Date) = date.getTime
+      val expected = new GregorianCalendar(2010, JANUARY, 1).getTime
+      assert(getTimeFromDate("2010/01/01") === expected.getTime)
     }
   }
 }
